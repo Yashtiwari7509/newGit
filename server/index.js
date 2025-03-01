737da4d9a90c9e6853d8944f0b847d6d29bc2499
YASH <pickwickjohn4@gmail.com>
2025-03-02 00:30:02 +0530

    yash

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import http from "http";
 import app from "./app.js";
-import { Server } from "socket.io";
 import { setupSocketIO } from "./chat/chathandler.controller.js";
 
 const PORT = process.env.PORT || 8000;
